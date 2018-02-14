959e41e4237fe97aa17dc12cea7415f100a9429c
patrickatcw <pj8392@yahoo.com>
2018-02-14 09:26:32 -0500

    program structure, directions, gitignore update

---
+++ src/AddMult.java
@@ -0,0 +1,14 @@
+/*
+Directions
+Write a program that can add any two integers using a method called
+add and can multiply any two integers using a method called multiply
+*/
+
+
+public class AddMult {
+
+    public static void main(String[] args) {
+
+    }
+
+}
