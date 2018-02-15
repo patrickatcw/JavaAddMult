37642e8447ea9b23c9c1a105458f87a9ca329bec
patrickatcw <pj8392@yahoo.com>
2018-02-14 21:10:43 -0500

    created Add and Multiply methods, program runs correctly

--- src/AddMult.java
+++ src/AddMult.java
@@ -2,12 +2,45 @@
 Directions
 Write a program that can add any two integers using a method called
 add and can multiply any two integers using a method called multiply
+First, do it with methods in the same class as your
+“public static void main(String[] args)” method.
+Then, make a new class called “Calculator” with those methods and use
+the calculator object to do the adding and multiplying.
 */
 
+import java.util.Scanner;
 
 public class AddMult {
 
     public static void main(String[] args) {
+
+        Add();
+
+        Multiply();
+
+    }
+
+    public static void Add(){
+
+        int a, b, c;
+        System.out.println("Enter two integers to calculate their sum ");
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+        c = a + b;
+        System.out.println("Sum of entered integers = " + c);
+
+    }
+
+    public static void Multiply(){
+
+        int x, y, z;
+        System.out.println("Enter two integers to multiply them ");
+        Scanner in = new Scanner(System.in);
+        x = in.nextInt();
+        y = in.nextInt();
+        z = x * y;
+        System.out.println("Multiplied integers = " + z);
 
     }
 
